11335b39a83dada7b0c30a49e6345405c8bede3a
suhwoo <rbfrnltls@gmail.com>
2025-05-28 16:11:15 +0900

    회원서비스 - 비지니스 Service 개발

---
+++ hello-spring/hello-spring/src/main/java/hello/hello_spring/service/MemberService.java
@@ -0,0 +1,35 @@
+package hello.hello_spring.service;
+
+import hello.hello_spring.domain.Member;
+import hello.hello_spring.repository.MemberRepository;
+import hello.hello_spring.repository.MemoryMemberRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public class MemberService {
+//서비스클래스는 보통 기능과 관련된 이름으로 써야한다.(비지니스이기 때문)
+    private final MemberRepository memberRepository;
+
+    public MemberService(MemberRepository memberRepository){
+        this.memberRepository = memberRepository;
+    }
+
+    public Long join(Member member){
+        //같은 이름의 회원 x
+        Optional<Member> result = memberRepository.finalByName(member.getName());
+        result.ifPresent(m -> {
+            throw new IllegalStateException("이미 존재하는 회원입니다");
+        });
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    public List<Member> findMembers(){
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> findOne(Long memberId){
+        return memberRepository.findById(memberId);
+    }
+}
